5fffd5df6370c6aaebf007d3ca2f4250c0709940
web <35491594+gws0920@users.noreply.github.com>
2022-07-11 15:34:29 +0800

    feat: 灯光

--- src/components/LightSettings.vue
+++ src/components/LightSettings.vue
@@ -15,7 +15,7 @@ export default {
     }
   },
   mounted() {
-    this.init()
+    this.init(true)
   },
   methods: {
     init(showHelper = false) {
@@ -32,30 +32,40 @@ export default {
       const camera = new THREE.PerspectiveCamera(35, innerWidth / innerHeight, 1, 1000)
       camera.position.set(160, 40, 10)
 
+      // 灯光
       const spotLight = this.renderLight()
       scene.add( spotLight );
       // 帮助线条
       const { lightHelper, shadowCameraHelper } = showHelper ? this.helpers(scene, { light: spotLight }) : {}
 
+      // 地面
       let material = new THREE.MeshPhongMaterial( { color: 0x808080, dithering: true } );
-
       let geometry = new THREE.PlaneGeometry( 2000, 2000 );
-
       let mesh = new THREE.Mesh( geometry, material );
       mesh.position.set( 0, - 1, 0 );
       mesh.rotation.x = - Math.PI * 0.5;
       mesh.receiveShadow = true;
       scene.add( mesh );
 
+      // 长方体
       material = new THREE.MeshPhongMaterial( { color: 0x4080ff, dithering: true } );
-
       geometry = new THREE.BoxGeometry(15, 3, 15);
-
       mesh = new THREE.Mesh( geometry, material );
       mesh.position.set( 0, 5, 0 );
       mesh.castShadow = true;
       scene.add( mesh );
 
+      // 模拟光线圆锥
+      // geometry = new THREE.ConeGeometry(50, 50, 32)
+      // geometry.rotateX(Math.PI * 0.115)
+      // material = new THREE.MeshBasicMaterial( { color: 'transparent', dithering: true } );
+      // material.alphaMap = 0x000000
+      // mesh = new THREE.Mesh( geometry, material );
+      // mesh.position.set( 0, 16, 6 );
+      // // mesh.castShadow = true;
+      // scene.add( mesh );
+      // console.log(geometry)
+  
       const controls = new OrbitControls( camera, renderer.domElement );
       controls.addEventListener( 'change', render );
       controls.minDistance = 20;
@@ -75,7 +85,7 @@ export default {
       controls.reset()
     },
     renderLight() {
-      const spotLight = new THREE.SpotLight( 0xffffff, 1 );
+      const spotLight = new THREE.SpotLight( 0xffffff );
       spotLight.position.set( 0, 40, 15 );
       spotLight.angle = Math.PI / 4;
       spotLight.penumbra = 0.1;
@@ -94,13 +104,17 @@ export default {
       // 帮助线条
       const lightHelper = new THREE.SpotLightHelper( light ); // 灯光 helper
       scene.add( lightHelper );
-      const axesHelper = new THREE.AxesHelper( 50 ); // 坐标轴 helper
-      axesHelper.setColors(0xff0000, 0xffff00, 0x00ffff) // x, y, z
-      scene.add( axesHelper );
-      const shadowCameraHelper = new THREE.CameraHelper( light.shadow.camera ); // 相机 helper
-      scene.add( shadowCameraHelper );
+      // const axesHelper = new THREE.AxesHelper( 50 ); // 坐标轴 helper
+      // axesHelper.setColors(0xff0000, 0xffff00, 0x00ffff) // x, y, z
+      // scene.add( axesHelper );
+      // const shadowCameraHelper = new THREE.CameraHelper( light.shadow.camera ); // 相机 helper
+      // scene.add( shadowCameraHelper );
 
-      return { lightHelper, axesHelper, shadowCameraHelper }
+      return { 
+        lightHelper,
+        //  axesHelper, 
+        //  shadowCameraHelper 
+        }
     }
   }
 }
